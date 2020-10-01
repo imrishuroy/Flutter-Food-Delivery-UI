c13d8fb10dce0f73d96d60bece568e1556aee448
Rishu Roy <rishukumar.prince@gmail.com>
2020-10-01 14:38:32 +0530

    Added AppBar And TextField

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import '../data/data.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Food UI'),
+        leading: Icon(Icons.account_circle),
+        actions: [
+          FlatButton(
+            onPressed: () {},
+            child: Text(
+              'Cart (${currentUser.cart.length})',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+              ),
+            ),
+          )
+        ],
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      30.0,
+                    ),
+                    borderSide: BorderSide(width: 0.8),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      30.0,
+                    ),
+                    borderSide: BorderSide(
+                      width: 0.8,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.search,
+                  ),
+                  suffixIcon: Icon(Icons.clear),
+                  hintText: 'Search Food Or Restaurant'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
